df50ca1cab549e80041f80e340c233f65f226520
fvaldes0109 <fvaldes0109@gmail.com>
2023-03-06 19:07:26 -0500

    Add Readme

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ export default class App extends React.Component {
       <div className="App">
         <header>
           <h1>Where in the world?</h1>
-          <div className="mode-toggle">
+          {/* <div className="mode-toggle">
             <img src="https://img.icons8.com/ios/50/000000/moon-symbol.png" alt="moon icon" width="20"/>
             <p>Dark Mode</p>
-          </div>
+          </div> */}
         </header>
     
         <BrowserRouter>
